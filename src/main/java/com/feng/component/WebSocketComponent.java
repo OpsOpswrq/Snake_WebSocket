76b9e3e4b0b91a2aecc3d1f9ce7574ec86f127aa
“wrq” <sadpigwan@foxmail.com>
2023-04-03 00:11:50 +0800

    烂的一b完成了

--- src/main/java/com/feng/component/WebSocketComponent.java
+++ src/main/java/com/feng/component/WebSocketComponent.java
@@ -1,10 +1,14 @@
 package com.feng.component;
 
 import cn.hutool.core.date.DateUtil;
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONObject;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.PathParam;
@@ -13,21 +17,68 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.CopyOnWriteArraySet;
 
-//@Component
+@Component
 @Slf4j
 @ServerEndpoint(value = "/game/{userId}")
 public class WebSocketComponent {
-    private static final CopyOnWriteArraySet<Session> SESSIONS = new CopyOnWriteArraySet<>();
-    private static final Map<String,Session> SESSION_POOL = new HashMap<>();
+    public static CopyOnWriteArraySet<Session> SESSIONS = new CopyOnWriteArraySet<>();
+    public static Map<String,Session> SESSION_POOL = new HashMap<>();
 
     @OnOpen
-    private void onOpen(Session session, @PathParam(value = "userId") String userId){
+    public void onOpen(Session session, @PathParam(value = "userId") String userId){
         try {
             SESSION_POOL.put(userId,session);
             SESSIONS.add(session);
+
             log.info("欢迎用户{},进入游戏，现在时间:{}",userId,DateUtil.now());
         }catch (Exception e){
             log.error("出现错误,错误原因:{},现在时间是:{}",e.getMessage(), DateUtil.now());
+        }
+    }
+    @OnClose
+    public void onClose(Session session,@PathParam(value = "userId") String userId){
+        try{
+            SESSIONS.remove(session);
+            SESSION_POOL.remove(userId);
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("userIds",SESSION_POOL.keySet());
+            sendAllMessage(jsonObject.toJSONString());
+            log.info("下次光临");
+        }catch (Exception e){
+            log.error("出错错误,错误信息:{},现在时间:{}",e.getMessage(),DateUtil.now());
+        }
+    }
+    @OnMessage
+    public void onMessage(String message){
+        JSONObject jsonObject = JSON.parseObject(message);
+        log.info(message);
+        if(jsonObject.containsKey("toUserId")){
+            sendOneMessage(jsonObject.getString("toUserId"),jsonObject.getString("fromUserId"));
+        }
+    }
+    public void sendAllMessage(String message) {
+        log.info("【WebSocket消息】广播消息：" + message);
+        for (Session session : SESSIONS) {
+            try {
+                if (session.isOpen()) {
+                    session.getAsyncRemote().sendText(message);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    public void sendOneMessage(String userId, String message) {
+        Session session = SESSION_POOL.get(userId);
+        if (session != null && session.isOpen()) {
+            try {
+                synchronized (session) {
+                    log.info("【WebSocket消息】单点消息：" + message);
+                    session.getAsyncRemote().sendText(message);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 }
--- src/main/java/com/feng/controller/F102.java
+++ src/main/java/com/feng/controller/F102.java
@@ -1,25 +1,27 @@
 package com.feng.controller;
 
 import cn.hutool.core.date.DateUtil;
+import cn.hutool.json.JSON;
+import com.alibaba.fastjson2.JSONObject;
 import com.feng.bean.CommonResult;
 import com.feng.component.WebSocketComponent;
 import com.feng.utils.VHelper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.websocket.server.PathParam;
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
 @Slf4j
 public class F102 {
-    private static Map<String,String> userIds = new HashMap<>();
-    @GetMapping("/main")
-    public String F101(){
+    @Autowired
+    WebSocketComponent webSocketComponent;
+//    private static Map<String,String> userIds = new HashMap<>();
+    @GetMapping("/main/{userId}")
+    public String F101(@PathVariable String userId){
         String result = "空";
         try{
             VHelper vh = new VHelper("T/F101/index.html");
@@ -40,13 +42,36 @@ public class F102 {
         }
         return result;
     }
+    @GetMapping("/ajax2")
+    public CommonResult ajax2(){
+        CommonResult result = new CommonResult();
+        try{
+            result.setData(webSocketComponent.SESSION_POOL.keySet());
+            result.setCode(200);
+            result.setMsg("success");
+            log.info("/ajax2 执行成功,现在时间是:{}",DateUtil.now());
+        }catch (Exception e){
+            log.error("/ajax2 执行错误,错误信息:{},现在时间是:{}",e.getMessage(),DateUtil.now());
+        }
+        return result;
+    }
+    @PostMapping("/ajax3")
+    public void ajax3(){
+        try{
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("userIds",webSocketComponent.SESSION_POOL.keySet());
+            webSocketComponent.sendAllMessage(jsonObject.toJSONString());
+            log.info("/ajax3 发送信息:{},现在时间:{}",webSocketComponent.SESSION_POOL.keySet(),DateUtil.now());
+        }catch (Exception e){
+            log.error("/ajax3 执行错误,错误信息:{},现在时间:{}",e.getMessage(),DateUtil.now());
+        }
+    }
     @PostMapping("/F102/ajax1")
     public CommonResult ajax1(@RequestBody Map<String,Object> params){
         CommonResult result = new CommonResult();
         try{
             if(params != null){
                 String userId = params.get("userId").toString().trim();
-                userIds.put(userId,userId);
                 result.setCode(200);
                 result.setMsg("success");
                 log.info("/F102/ajax1 执行成功,参数信息:{},现在时间:{}",params,DateUtil.now());
